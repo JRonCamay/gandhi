22e8226d1e19fcf603737fad32115d371660173d
JRonCamay <jron.camay@gmail.com>
2026-06-26 12:34:47 +0800

    Update JTransformPlus.js

--- JTransformPlus.js
+++ JTransformPlus.js
@@ -1,185 +1,167 @@
-(function(Scratch) {
-    'use strict';
+(function (Scratch) {
+    "use strict";
 
     if (!Scratch.extensions.unsandboxed) {
         throw new Error(
-            "Transform Extension requires unsandboxed mode"
-        );
-    }
-class TransformExtension {
-
-    getInfo() {
-        return {
-            id: "transform",
-            name: "Transform",
-
-            color1: "#9966FF",
-            color2: "#8A55E6",
-
-            blocks: [
-
-                {
-                    opcode: "flipHorizontal",
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: "flip sprite horizontally"
-                },
-
-                {
-                    opcode: "flipVertical",
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: "flip sprite vertically"
-                },
-
-                {
-                    opcode: "rotateBy",
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: "rotate sprite by [ANGLE] degrees",
-                    arguments: {
-                        ANGLE: {
-                            type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 15
-                        }
-                    }
-                },
-
-                "---",
-
-                {
-                    opcode: "setWidth",
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: "set sprite width to [WIDTH]",
-                    arguments: {
-                        WIDTH: {
-                            type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 100
-                        }
-                    }
-                },
-
-                {
-                    opcode: "setHeight",
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: "set sprite height to [HEIGHT]",
-                    arguments: {
-                        HEIGHT: {
-                            type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 100
-                        }
-                    }
-                },
-
-                "---",
-
-                {
-                    opcode: "getWidth",
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: "sprite width"
-                },
-
-                {
-                    opcode: "getHeight",
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: "sprite height"
-                }
-            ]
-        };
-    }
-
-    getDrawable(util) {
-
-        const target =
-            util.target;
-
-        return Scratch.vm.runtime
-            .renderer
-            ._allDrawables[
-                target.drawableID
-            ];
-    }
-
-    flipHorizontal(args, util) {
-
-        const target =
-            util.target;
-
-        target.setDirection(
-            180 - target.direction
+            "Transform Extension requires Unsandboxed Mode"
         );
     }
 
-    flipVertical(args, util) {
+    class TransformExtension {
 
-        const drawable =
-            this.getDrawable(
-                util
+        getInfo() {
+            return {
+                id: "transform",
+                name: "Transform",
+
+                color1: "#9966FF",
+                color2: "#8A55E6",
+
+                blocks: [
+
+                    {
+                        opcode: "flipHorizontal",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "flip sprite horizontally"
+                    },
+
+                    {
+                        opcode: "flipVertical",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "flip sprite vertically"
+                    },
+
+                    {
+                        opcode: "rotateBy",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "rotate sprite by [ANGLE] degrees",
+                        arguments: {
+                            ANGLE: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 15
+                            }
+                        }
+                    },
+
+                    "---",
+
+                    {
+                        opcode: "getDirection",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "sprite direction"
+                    }
+                ]
+            };
+        }
+
+        getDrawable(util) {
+
+            const target =
+                util.target;
+
+            if (!target) {
+                console.warn(
+                    "[Transform] No target"
+                );
+                return null;
+            }
+
+            const renderer =
+                Scratch.vm?.runtime?.renderer;
+
+            if (!renderer) {
+                console.warn(
+                    "[Transform] Renderer not found"
+                );
+                return null;
+            }
+
+            return renderer
+                ._allDrawables[
+                    target.drawableID
+                ];
+        }
+
+        flipHorizontal(args, util) {
+
+            try {
+
+                const target =
+                    util.target;
+
+                target.setDirection(
+                    180 -
+                    target.direction
+                );
+
+            } catch (err) {
+
+                console.error(
+                    "[Transform] flipHorizontal",
+                    err
+                );
+            }
+        }
+
+        flipVertical(args, util) {
+
+            try {
+
+                const drawable =
+                    this.getDrawable(
+                        util
+                    );
+
+                if (!drawable) return;
+
+                drawable.updateScale([
+                    drawable.scale[0],
+                    -drawable.scale[1]
+                ]);
+
+            } catch (err) {
+
+                console.error(
+                    "[Transform] flipVertical",
+                    err
+                );
+            }
+        }
+
+        rotateBy(args, util) {
+
+            try {
+
+                const target =
+                    util.target;
+
+                target.setDirection(
+                    target.direction +
+                    Number(
+                        args.ANGLE
+                    )
+                );
+
+            } catch (err) {
+
+                console.error(
+                    "[Transform] rotateBy",
+                    err
+                );
+            }
+        }
+
+        getDirection(args, util) {
+
+            return (
+                util.target
+                    ?.direction ?? 0
             );
-
-        drawable.updateScale([
-            drawable.scale[0],
-            -drawable.scale[1]
-        ]);
+        }
     }
 
-    rotateBy(args, util) {
-
-        const target =
-            util.target;
-
-        target.setDirection(
-            target.direction +
-            Number(args.ANGLE)
-        );
-    }
-
-    setWidth(args, util) {
-
-        const drawable =
-            this.getDrawable(
-                util
-            );
-
-        drawable.updateScale([
-            Number(args.WIDTH),
-            drawable.scale[1]
-        ]);
-    }
-
-    setHeight(args, util) {
-
-        const drawable =
-            this.getDrawable(
-                util
-            );
-
-        drawable.updateScale([
-            drawable.scale[0],
-            Number(args.HEIGHT)
-        ]);
-    }
-
-    getWidth(args, util) {
-
-        const drawable =
-            this.getDrawable(
-                util
-            );
-
-        return drawable.scale[0];
-    }
-
-    getHeight(args, util) {
-
-        const drawable =
-            this.getDrawable(
-                util
-            );
-
-        return drawable.scale[1];
-    }
-}
-
-Scratch.extensions.register(
-    new TransformExtension()
-);
+    Scratch.extensions.register(
+        new TransformExtension()
+    );
 
 })(Scratch);
