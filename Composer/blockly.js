a06bbbe0d14146e4457dc5e49eac75779d9bef93
JRonCamay <jron.camay@gmail.com>
2026-06-28 18:23:15 +0800

    Update blockly.js

--- Composer/blockly.js
+++ Composer/blockly.js
@@ -1,1 +1,41 @@
 
+// blockly.js
+
+(function () {
+
+    Composer.blockly = {};
+
+    function getWorkspace() {
+
+        if (typeof Blockly === "undefined")
+            return null;
+
+        return Blockly.getMainWorkspace();
+
+    }
+
+    Composer.blockly.workspace = getWorkspace;
+
+    Composer.blockly.create = function (type) {
+
+        const ws = getWorkspace();
+
+        if (!ws) {
+
+            console.error("Blockly workspace not found.");
+
+            return null;
+
+        }
+
+        const block = ws.newBlock(type);
+
+        block.initSvg();
+
+        block.render();
+
+        return block;
+
+    };
+
+})();
