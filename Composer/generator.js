948a4ebf1119688c1313eae8d2edd9415e6950df
JRonCamay <jron.camay@gmail.com>
2026-06-28 17:38:49 +0800

    Update generator.js

--- Composer/generator.js
+++ Composer/generator.js
@@ -1,3 +1,12 @@
 window.ComposerGenerator = {};
 
 console.log("generator loaded");
+Composer.generator.generate = function(text){
+
+    console.clear();
+
+    console.log("===== Gandhi Composer =====");
+
+    console.log(text);
+
+};
