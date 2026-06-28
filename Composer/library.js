935d38a83538e1dc5999e5aa094b5dcae9500860
JRonCamay <jron.camay@gmail.com>
2026-06-28 18:56:03 +0800

    Update library.js

--- Composer/library.js
+++ Composer/library.js
@@ -1,1 +1,43 @@
-console.log("library.js running");
+(function(){
+
+Composer.library = [
+
+{
+    keyword:"move",
+    block:"motion_movesteps",
+    label:"move steps"
+},
+
+{
+    keyword:"wait",
+    block:"control_wait",
+    label:"wait seconds"
+},
+
+{
+    keyword:"say",
+    block:"looks_say",
+    label:"say"
+},
+
+{
+    keyword:"turn",
+    block:"motion_turnright",
+    label:"turn clockwise"
+},
+
+{
+    keyword:"hide",
+    block:"looks_hide",
+    label:"hide"
+},
+
+{
+    keyword:"show",
+    block:"looks_show",
+    label:"show"
+}
+
+];
+
+})();
