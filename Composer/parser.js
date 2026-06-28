ab42b7205babaab02761d14b3b329020217f79af
JRonCamay <jron.camay@gmail.com>
2026-06-28 18:12:26 +0800

    Update parser.js

--- Composer/parser.js
+++ Composer/parser.js
@@ -1,1 +1,127 @@
-console.log("parser.js running");
+// parser.js
+
+(function () {
+
+    const parser = Composer.parser;
+
+    parser.parse = function (text) {
+
+        const ast = [];
+
+        const lines = text
+            .split("\n")
+            .map(x => x.trim())
+            .filter(x => x.length);
+
+        for (const line of lines) {
+
+            const node = parseLine(line);
+
+            if (node)
+                ast.push(node);
+
+        }
+
+        return ast;
+
+    };
+
+    function parseLine(line) {
+
+        let m;
+
+        m = line.match(/^move\s+(-?\d+(\.\d+)?)$/i);
+
+        if (m) {
+
+            return {
+
+                type: "move",
+
+                args: [Number(m[1])]
+
+            };
+
+        }
+
+        m = line.match(/^wait\s+(-?\d+(\.\d+)?)$/i);
+
+        if (m) {
+
+            return {
+
+                type: "wait",
+
+                args: [Number(m[1])]
+
+            };
+
+        }
+
+        m = line.match(/^turn\s+(-?\d+(\.\d+)?)$/i);
+
+        if (m) {
+
+            return {
+
+                type: "turn",
+
+                args: [Number(m[1])]
+
+            };
+
+        }
+
+        m = line.match(/^say\s+(.+)$/i);
+
+        if (m) {
+
+            return {
+
+                type: "say",
+
+                args: [m[1]]
+
+            };
+
+        }
+
+        m = line.match(/^hide$/i);
+
+        if (m) {
+
+            return {
+
+                type: "hide",
+
+                args: []
+
+            };
+
+        }
+
+        m = line.match(/^show$/i);
+
+        if (m) {
+
+            return {
+
+                type: "show",
+
+                args: []
+
+            };
+
+        }
+
+        return {
+
+            type: "unknown",
+
+            text: line
+
+        };
+
+    };
+
+})();
