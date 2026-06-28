1aa1f5269e2ac804403a9c35624c7adaccf360a0
JRonCamay <jron.camay@gmail.com>
2026-06-28 20:36:19 +0800

    Update library.js

--- Composer/library.js
+++ Composer/library.js
@@ -1,41 +1,84 @@
-(function(){
+// library.js
+
+(function () {
 
 Composer.library = [
 
 {
-    keyword:"move",
-    block:"motion_movesteps",
-    label:"move steps"
+    id: "motion_gotoxy",
+    block: "motion_gotoxy",
+    pattern: "go to x: [] y: []",
+    preview: "go to x: () y: ()"
 },
 
 {
-    keyword:"wait",
-    block:"control_wait",
-    label:"wait seconds"
+    id: "motion_movesteps",
+    block: "motion_movesteps",
+    pattern: "move [] steps",
+    preview: "move () steps"
 },
 
 {
-    keyword:"say",
-    block:"looks_say",
-    label:"say"
+    id: "motion_turnright",
+    block: "motion_turnright",
+    pattern: "turn clockwise [] degrees",
+    preview: "turn ↻ () degrees"
 },
 
 {
-    keyword:"turn",
-    block:"motion_turnright",
-    label:"turn clockwise"
+    id: "motion_turnleft",
+    block: "motion_turnleft",
+    pattern: "turn counterclockwise [] degrees",
+    preview: "turn ↺ () degrees"
 },
 
 {
-    keyword:"hide",
-    block:"looks_hide",
-    label:"hide"
+    id: "motion_goto",
+    block: "motion_goto",
+    pattern: "go to []",
+    preview: "go to ()"
 },
 
 {
-    keyword:"show",
-    block:"looks_show",
-    label:"show"
+    id: "motion_glidesecstoxy",
+    block: "motion_glidesecstoxy",
+    pattern: "glide [] secs to x: [] y: []",
+    preview: "glide () secs to x: () y: ()"
+},
+
+{
+    id: "looks_say",
+    block: "looks_say",
+    pattern: "say []",
+    preview: "say ()"
+},
+
+{
+    id: "looks_think",
+    block: "looks_think",
+    pattern: "think []",
+    preview: "think ()"
+},
+
+{
+    id: "looks_show",
+    block: "looks_show",
+    pattern: "show",
+    preview: "show"
+},
+
+{
+    id: "looks_hide",
+    block: "looks_hide",
+    pattern: "hide",
+    preview: "hide"
+},
+
+{
+    id: "control_wait",
+    block: "control_wait",
+    pattern: "wait [] seconds",
+    preview: "wait () seconds"
 }
 
 ];
