ca4ef4199c758129ff5adb9cdde394399d2ed8b7
JRonCamay <jron.camay@gmail.com>
2026-07-02 13:26:13 +0800

    Add simulation drag patch shell

---
+++ Transfork/engine/simulation-drag-patch.js
@@ -0,0 +1,13 @@
+/*
+Transfork/engine/simulation-drag-patch.js
+Patch shell for simulation-driven dragging.
+*/
+
+(function () {
+    'use strict';
+
+    if (window.__TransforkSimulationDragPatchLoaded) return;
+    window.__TransforkSimulationDragPatchLoaded = true;
+
+    console.log('[Transfork engine] simulation drag patch loaded');
+})();
