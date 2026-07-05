0aa71c4d43a6b3491a725cdaf614a732a96c9e28
JRonCamay <jron.camay@gmail.com>
2026-07-05 14:58:15 +0800

    Load Transfork modules from loader

--- Transfork/TransforkLoader.user.js
+++ Transfork/TransforkLoader.user.js
@@ -1,14 +1,30 @@
 // ==UserScript==
 // @name         Gandhi Transfork Modular Loader
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Loads modular Transfork files
 // @match        *://www.cocrea.world/*
 // @grant        none
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/state.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/drawable.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/math.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformOps.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDrag.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/resize.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/rotate.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/alpha.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/flip.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/skew.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/legacyBridge.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    console.log("Install module files listed in Transfork/MIGRATION.md while cutover is active.");
+    console.log("Gandhi Transfork modular loader active.");
 })();
