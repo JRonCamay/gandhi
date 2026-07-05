3f551733a2e9d0739f16604760bef15548e66073
JRonCamay <jron.camay@gmail.com>
2026-07-05 13:26:35 +0800

    Bump File Splitter loader require cache

--- Transfork/FileSplitterLoader.user.js
+++ Transfork/FileSplitterLoader.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Gandhi File Splitter Loader
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Loads Gandhi File Splitter from the Gandhi repository
 // @match        *://chatgpt.com/*
 // @match        *://github.com/*
 // @match        *://raw.githubusercontent.com/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/FileSplitter.user.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/FileSplitter.user.js?v=0.5
 // ==/UserScript==
 
 (function () {
