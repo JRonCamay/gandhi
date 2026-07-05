8a61d1677c5203746083d4ff3f3df0cb039f4350
JRonCamay <jron.camay@gmail.com>
2026-07-05 15:28:19 +0800

    Bust Transfork module cache

--- Transfork/TransforkLoader.user.js
+++ Transfork/TransforkLoader.user.js
@@ -1,30 +1,30 @@
 // ==UserScript==
 // @name         Gandhi Transfork Modular Loader
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Loads modular Transfork files
 // @match        *://www.cocrea.world/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/state.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/drawable.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/math.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformOps.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDrag.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/resize.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/rotate.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/alpha.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/flip.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/skew.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/legacyBridge.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/state.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/drawable.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/math.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformOps.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDrag.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/resize.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/rotate.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/alpha.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/flip.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/skew.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/legacyBridge.js?v=26070504
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js?v=26070504
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    console.log("Gandhi Transfork modular loader active.");
+    console.log("Gandhi Transfork modular loader active 0.4.");
 })();
