f166b03cd8a7a3b302f736100f4aec77c690c290
JRonCamay <jron.camay@gmail.com>
2026-07-05 12:10:21 +0800

    Load expanded Transfork modules

--- Transfork/TransforkLoader.user.js
+++ Transfork/TransforkLoader.user.js
@@ -1,22 +1,14 @@
 // ==UserScript==
 // @name         Gandhi Transfork Modular Loader
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Loads modular Transfork files in dependency order
+// @version      0.2
+// @description  Loads modular Transfork files
 // @match        *://www.cocrea.world/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDrag.js
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    if (!window.Transfork || !window.Transfork.main) {
-        console.error("Transfork modular loader failed to load modules.");
-    }
+    console.log("Install module files listed in Transfork/MIGRATION.md while cutover is active.");
 })();
