14af4875429e9b1a6467e18c3971502227b86ed3
JRonCamay <jron.camay@gmail.com>
2026-07-05 20:12:59 +0800

    Bump resize snapshot fix cache

--- Transfork/TransforkLoader.user.js
+++ Transfork/TransforkLoader.user.js
@@ -1,37 +1,37 @@
 // ==UserScript==
 // @name         Gandhi Transfork Modular Loader
 // @namespace    http://tampermonkey.net/
-// @version      1.14
+// @version      1.15
 // @description  Loads modular Transfork files
 // @match        *://www.cocrea.world/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/state.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/drawable.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/math.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformOps.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotLayer.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/pixelBounds.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformSnapshotGuard.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/overlayTop.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapGuideOverlay.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/pixelBoxSync.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDragPixel.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotToolsPixel.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/resize.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/rotate.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/alpha.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/flip.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/skew.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/legacyBridge.js?v=26070525
-// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js?v=26070525
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/namespace.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/state.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/vm.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/coords.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/drawable.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/math.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/selectionBox.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformOps.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotLayer.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/pixelBounds.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/transformSnapshotGuard.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/overlayTop.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapGuideOverlay.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/pixelBoxSync.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotDragPixel.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/snapshotToolsPixel.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/resize.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/rotate.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/alpha.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/flip.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/skew.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/legacyBridge.js?v=26070526
+// @require      https://raw.githubusercontent.com/JRonCamay/gandhi/main/Transfork/main.js?v=26070526
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    console.log("Gandhi Transfork modular loader active 1.14.");
+    console.log("Gandhi Transfork modular loader active 1.15.");
 })();
