c4a82d9625dd5633c3ac8222a6fa7174c6689751
JRonCamay <jron.camay@gmail.com>
2026-07-06 23:52:49 +0800

    Add flip vertical button click skeleton

---
+++ TransforkNew/UI/ELEMENTS/BUTTONS/FLIPVBUTTON/click.js
@@ -0,0 +1,20 @@
+window.TransforkNew = window.TransforkNew || {};
+window.TransforkNew.UI = window.TransforkNew.UI || {};
+window.TransforkNew.UI.elements = window.TransforkNew.UI.elements || {};
+window.TransforkNew.UI.elements.buttons = window.TransforkNew.UI.elements.buttons || {};
+window.TransforkNew.UI.elements.buttons.FLIPVBUTTON = window.TransforkNew.UI.elements.buttons.FLIPVBUTTON || {};
+
+(function () {
+    "use strict";
+
+    function click(button) {
+        if (!button?.node) return null;
+        button.node.addEventListener("click", event => {
+            event.preventDefault();
+            event.stopPropagation();
+        }, true);
+        return button.node;
+    }
+
+    window.TransforkNew.UI.elements.buttons.FLIPVBUTTON.click = click;
+})();
