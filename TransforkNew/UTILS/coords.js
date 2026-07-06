f3d4d457e72e2d54bf90be7b9f17d1180d1f8fb2
JRonCamay <jron.camay@gmail.com>
2026-07-07 00:47:51 +0800

    Add coords object skeleton

---
+++ TransforkNew/UTILS/coords.js
@@ -0,0 +1,8 @@
+window.TransforkNew = window.TransforkNew || {};
+window.TransforkNew.UTILS = window.TransforkNew.UTILS || {};
+
+(function () {
+    "use strict";
+
+    window.TransforkNew.UTILS.coords = window.TransforkNew.UTILS.coords || {};
+})();
