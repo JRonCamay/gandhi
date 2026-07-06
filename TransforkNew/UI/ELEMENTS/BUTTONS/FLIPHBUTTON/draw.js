e25e89e0b97dbe965c732d3c1895a8cc71f7ffaf
JRonCamay <jron.camay@gmail.com>
2026-07-06 23:51:12 +0800

    Add flip horizontal button draw skeleton

---
+++ TransforkNew/UI/ELEMENTS/BUTTONS/FLIPHBUTTON/draw.js
@@ -0,0 +1,33 @@
+window.TransforkNew = window.TransforkNew || {};
+window.TransforkNew.UI = window.TransforkNew.UI || {};
+window.TransforkNew.UI.elements = window.TransforkNew.UI.elements || {};
+window.TransforkNew.UI.elements.buttons = window.TransforkNew.UI.elements.buttons || {};
+window.TransforkNew.UI.elements.buttons.FLIPHBUTTON = window.TransforkNew.UI.elements.buttons.FLIPHBUTTON || {};
+
+(function () {
+    "use strict";
+
+    function draw(button) {
+        if (!button?.node) return null;
+        button.node.textContent = "⇋";
+        Object.assign(button.node.style, {
+            position: "absolute",
+            left: "-26px",
+            top: "0px",
+            width: "20px",
+            height: "20px",
+            borderRadius: "4px",
+            background: "#8e44ad",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            pointerEvents: "auto",
+            fontSize: "14px"
+        });
+        return button.node;
+    }
+
+    window.TransforkNew.UI.elements.buttons.FLIPHBUTTON.draw = draw;
+})();
