0e2a7ed1eb7cc488f40c700175d85d50aff5fa8a
JRonCamay <jron.camay@gmail.com>
2026-07-06 23:23:25 +0800

    Add rotate button mouseDown method skeleton

---
+++ TransforkNew/UI/ELEMENTS/BUTTONS/ROTATEBUTTON/mouseDown.js
@@ -0,0 +1,20 @@
+window.TransforkNew = window.TransforkNew || {};
+window.TransforkNew.UI = window.TransforkNew.UI || {};
+window.TransforkNew.UI.elements = window.TransforkNew.UI.elements || {};
+window.TransforkNew.UI.elements.buttons = window.TransforkNew.UI.elements.buttons || {};
+window.TransforkNew.UI.elements.buttons.ROTATEBUTTON = window.TransforkNew.UI.elements.buttons.ROTATEBUTTON || {};
+
+(function () {
+    "use strict";
+
+    function mouseDown(button) {
+        if (!button?.node) return null;
+        button.node.addEventListener("mousedown", event => {
+            event.preventDefault();
+            event.stopPropagation();
+        }, true);
+        return button.node;
+    }
+
+    window.TransforkNew.UI.elements.buttons.ROTATEBUTTON.mouseDown = mouseDown;
+})();
