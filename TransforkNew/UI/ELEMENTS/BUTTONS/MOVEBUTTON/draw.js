2fc5f3361fe7d613128912b5bf0f5c728ca17484
JRonCamay <jron.camay@gmail.com>
2026-07-06 23:31:05 +0800

    Add move button draw method skeleton

---
+++ TransforkNew/UI/ELEMENTS/BUTTONS/MOVEBUTTON/draw.js
@@ -0,0 +1,34 @@
+window.TransforkNew = window.TransforkNew || {};
+window.TransforkNew.UI = window.TransforkNew.UI || {};
+window.TransforkNew.UI.elements = window.TransforkNew.UI.elements || {};
+window.TransforkNew.UI.elements.buttons = window.TransforkNew.UI.elements.buttons || {};
+window.TransforkNew.UI.elements.buttons.MOVEBUTTON = window.TransforkNew.UI.elements.buttons.MOVEBUTTON || {};
+
+(function () {
+    "use strict";
+
+    function draw(button) {
+        if (!button?.node) return null;
+        button.node.textContent = "✥";
+        Object.assign(button.node.style, {
+            position: "absolute",
+            left: "50%",
+            top: "-23px",
+            width: "20px",
+            height: "20px",
+            marginLeft: "-10px",
+            borderRadius: "4px",
+            background: "#e53935",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "move",
+            pointerEvents: "auto",
+            fontSize: "16px"
+        });
+        return button.node;
+    }
+
+    window.TransforkNew.UI.elements.buttons.MOVEBUTTON.draw = draw;
+})();
